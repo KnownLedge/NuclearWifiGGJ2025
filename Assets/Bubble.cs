f63a30f65eefe852413dfb77a7b38e27b9add76a
IvyHearts79 <emcint303@caledonian.ac.uk>
2025-01-25 17:52:26 +0000

    base bubble movement

---
+++ Assets/Bubble.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Bubble : MonoBehaviour
+{
+    public float size = 1.0f;
+    float speed = 0.4f;
+    float directionY = 0.5f;
+
+    void FixedUpdate()
+    {
+        Vector3 scale = new Vector3();
+        scale.x = size;
+        scale.y = size;
+        transform.localScale = scale;
+
+        Vector3 position = transform.localPosition;
+        position.y += speed * directionY;
+        transform.localPosition = position;
+    }
+}
